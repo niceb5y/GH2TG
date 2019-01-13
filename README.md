dc58444c7be42d650b868d53cb3e94e20e25a053
Seungho Kim <niceb5y@gmail.com>
2019-01-14 00:29:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GH2TG
+
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+
+Forward [GitHub push webhook](https://developer.github.com/webhooks/) to [Telegram messenger](https://telegram.org) with [AWS Lambda](https://aws.amazon.com/lambda/).
---
+++ handler.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const axios = require('axios')
+const crypto = require('crypto')
+
+const secret = process.env.GH2TG_SECRET
+const tgToken = process.env.GH2TG_TG_TOKEN
+const tgChatId = process.env.GH2TG_TG_CHATID
+
+const sendTelegramMessage = async text => {
+  return axios.post(`https://api.telegram.org/bot${tgToken}/sendMessage`, {
+    chat_id: tgChatId,
+    text: text,
+    parse_mode: 'Markdown'
+  })
+}
+
+const verifyWebhook = (body, signature) => {
+  if (body === '' || signature === '') return false
+  const payload = JSON.stringify(body)
+  const hmac = crypto.createHmac('sha1', secret)
+  const digest = 'sha1=' + hmac.update(payload).digest('hex')
+  return digest === signature
+}
+
+module.exports.push = async (event, context) => {
+  const body = JSON.parse(event.body) || ''
+  const signature = event.headers['X-Hub-Signature'] || ''
+  const isValidRequest = verifyWebhook(body, signature)
+  if (!isValidRequest) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: 'Invalid request'
+      })
+    }
+  }
+  if (body.commits) {
+    const commits = body.commits || []
+    const commitMsg = commits
+      .map(commit => `*${commit.author.name}* - ${commit.message}`)
+      .join('\n')
+    const compareURL = body.compare ? `\n\n${body.compare}` : ''
+    await sendTelegramMessage(`${commitMsg}${compareURL}`)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Success'
+      })
+    }
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'No commits'
+      })
+    }
+  }
+}
